1f2e210451051056fa523d4f9c9b6a5c5f0a586c
Andy Mell <afmell@afmell.com>
2019-03-25 10:46:43 +0000

    Adding code to read maximum mats counts from the mats object

--- mats.py
+++ mats.py
@@ -8,6 +8,9 @@ class Mats:
         ref_file_name is the file location that contains all the reference
         information about materials and grades
         """
+        self._GRADES_KEY = "grades"
+        self._MAXIMUMS_KEY = "maximums"
+
         with open(ref_file_name, "rt") as ref_file:
             self._mats = json.load(ref_file)
 
@@ -19,5 +22,15 @@ class Mats:
         for m in self._mats['grades']:
             if m['name'] == material_name:
                 return m['grade']
+        return None
+
+    def maximum(self, material_name):
+        """
+        Returns the maximu storage for a material_name, or None if the 
+        name is not recognised
+        """
+        g = str(self.grade(material_name))
+        if g and g in self._mats['maximums']:
+            return self._mats['maximums'][g]
         return None
     
--- mats_test.py
+++ mats_test.py
@@ -11,6 +11,13 @@ class MatsTest(unittest.TestCase):
         self.assertEqual(1, m.grade("iron"))
         self.assertIsNone(m.grade("gold"))
 
+    def test_maximum(self):
+        m = mats.Mats("material_info.json")
+        self.assertEqual(100, m.maximum("dataminedwake"))
+        self.assertEqual(300, m.maximum("sulphur"))
+        self.assertEqual(None, m.maximum("gold"))
+       
+
 if __name__ == "__main__":
     unittest.main()
    
